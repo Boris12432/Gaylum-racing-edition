44d6403b5f45c97327c22c985ad6cd29d5cf7fab
Boris12432 <Andreipro192@gmail.com>
2024-02-24 21:34:52 +0600

    Цербер

---
+++ Networking/Explained/Multiplayer/control.gd
@@ -0,0 +1,23 @@
+extends Control
+
+ 
+var peer = ENetMultiplayerPeer.new()
+@export var player_scene: PackedScene
+ 
+ 
+func _on_host_pressed():
+	peer.create_server(1832)
+	multiplayer.multiplayer_peer = peer
+	multiplayer.peer_connected.connect(_add_player)
+	_add_player()
+	
+func _add_player(id = 1):
+	var player = player_scene.instantiate()
+	player.name = str(id)
+	call_deferred("add_child",player)
+ 
+ 
+func _on_join_pressed():
+	peer.create_client("localhost", 1832)
+	multiplayer.multiplayer_peer = peer
+	
---
+++ Networking/Game/README.md
@@ -0,0 +1,2 @@
+# TODO update README.
+Check out the project though, there's a server hosted at whale-game.mitchmakesthings.online if you want to connect to it :)
